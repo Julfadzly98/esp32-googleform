435e24fc70e973370e83bb4a26869c2710814826
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-04-03 14:33:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from boot import connect
 import urequests
 from time import sleep
 
+pin = machine.Pin(0, 1,  machine.Pin.OUT) #D2D4
+
 connect()
+
+
 
 while True:
   sleep(1) # do every 60 seconds
-
-  #led = 1
-  #hello = 1
   h = {'content-type' : 'application/x-www-form-urlencoded'}
   form_url = 'https://docs.google.com/forms/d/e/1FAIpQLSeIplALqZE19SQyhaeirgWAINPK5KjUn3uwXGOXT8QJfBM7IQ/formResponse?usp=pp_url&'
-  form_data = 'entry.561083855='+ str(1)
+  form_data = 'entry.561083855=' + 'entry.561083855=' str(pin)
   r = urequests.post(form_url, data=form_data, headers=h)
   r.status_code
   r.close()
